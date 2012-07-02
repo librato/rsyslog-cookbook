eb3a59e0a3189d359d464756befac6b01b6d56e0
Mike Heffner <mikeh@fesnel.com>
2012-07-02 11:36:14 -0400

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures rsyslog"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.100.2"
+version           "0.100.3"
 
 recipe            "rsyslog", "Installs rsyslog from source"
 recipe            "rsyslog::client", "Sets up a client to log to a remote rsyslog server"
